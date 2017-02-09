aec0405463d9752b03ed01f6affd164add96330b
andy <andy@intrepidfrog.com>
2017-02-09 09:32:33 +0000

    PromiseKit

--- ErikFutureTest/ViewController.swift
+++ ErikFutureTest/ViewController.swift
@@ -8,13 +8,16 @@
 
 import UIKit
 import Erik
-import BrightFutures
+
+//import BrightFutures
+
+import PromiseKit
 
 class ViewController: UIViewController {
     
     @IBAction func test(_ sender: Any) {
         
-        demoErikFutures()
+        demoErikPromiseKit()
     }
     
     override func viewDidLoad() {
@@ -28,38 +31,139 @@ class ViewController: UIViewController {
     }
 }
 
-func demoErikFutures() {
-    // Expected output:
-    //    1: Google
-    //    -: Microsoft – Official Home Page
-    //    2: Microsoft – Official Home Page DOM manipulated
-    //    3: Amazon.com
-    //    -: How people build software · GitHub
-    //    4: How people build software · GitHub DOM manipulated
+//func demoErikFutures() {
+//    // Expected output:
+//    //    1: Google
+//    //    -: Microsoft – Official Home Page
+//    //    2: Microsoft – Official Home Page DOM manipulated
+//    //    3: Amazon.com
+//    //    -: How people build software · GitHub
+//    //    4: How people build software · GitHub DOM manipulated
+//
+//    func erikPromise(url: URL) -> Future<String, NSError> {
+//        let promise = Erik.Promise<String, NSError>()
+//        
+//        let _ = Erik.visitFuture(url: url).andThen { result in
+//            
+//            switch result {
+//            case .success(let doc):
+//                print("-: \(doc.title!)")
+//                
+//                // do some things with the DOM
+//                
+//                promise.success("\(doc.title!) DOM manipulated")
+//                
+//            case .failure(let err):
+//                print("-: \(err)")
+//                promise.failure(err)
+//            }
+//            
+//        }
+//        
+//        return promise.future
+//    }
+//    
+//    let urlStrings = [
+//        "https://www.google.com",
+//        "https://www.microsoft.com",
+//        "https://www.amazon.com",
+//        "https://www.github.com"
+//    ]
+//    
+//    let urls = urlStrings.map { URL(string: $0)! }
+//    
+//    
+//    Erik.visit(urlString: urlStrings[0]) { doc, err in
+//        if err == nil {
+//            print("1: \(doc!.title!)")
+//        } else {
+//            print("1: \(err)")
+//        }
+//    }
+//    
+//    
+//    let _ = erikPromise(url: urls[1]).andThen { result in
+//        switch result {
+//        case .success(let message):
+//            print("2: \(message)")
+//            
+//        case .failure(let err):
+//            print("2. \(err)")
+//        }
+//    }
+//    
+//    let _ = Erik.visitFuture(url: urls[2])
+//        .andThen { result in
+//            switch result {
+//            case .success(let doc):
+//                print("3: \(doc.title!)")
+//                
+//            case .failure(let err):
+//                print("3: \(err)")
+//            }
+//        }.andThen { _ in
+//            erikPromise(url: urls[3])
+//                .onSuccess { message in
+//                    print("4: \(message)")
+//                }
+//                .onFailure { err in
+//                    print("4: \(err)")
+//                }
+//        }
+//}
 
-    func erikPromise(url: URL) -> Future<String, NSError> {
-        let promise = Promise<String, NSError>()
+extension Erik {
+    public func visitPromise(url: URL) -> Promise<Document> {
         
-        let _ = Erik.visitFuture(url: url).andThen { result in
-            
-            switch result {
-            case .success(let doc):
-                print("-: \(doc.title!)")
-                
-                // do some things with the DOM
-                
-                promise.success("\(doc.title!) DOM manipulated")
-                
-            case .failure(let err):
-                print("-: \(err)")
-                promise.failure(err)
+        return Promise { fulfill, reject in
+            let _ = Erik.visit(url: url) { doc, err in
+                if err == nil {
+                    fulfill(doc!)
+                } else {
+                    reject(err!)
+                }
             }
-            
         }
-        
-        return promise.future
     }
     
+    public static func visitPromise(url: URL) -> Promise<Document> {
+        return Erik.sharedInstance.visitPromise(url: url)
+    }
+
+}
+
+func demoErikPromiseKit() {
+    // Expected output:
+//        1: Optional("Google")
+//        -: Microsoft – Official Home Page
+//        Number of elements: xxx
+//        2: Microsoft – Official Home Page DOM manipulated
+//        3: Optional("Amazon.com")
+//        -: How people build software · GitHub
+//        Number of elements: xxx
+//        4: How people build software · GitHub DOM manipulated
+    
+    func manipulateDom(document: Document) {
+        print("Number of elements: \(document.elements.count)")
+    }
+    
+    func erikPromise(url: URL) -> Promise<String> {
+
+        let promise: Promise<String> = Erik.visitPromise(url: url)
+        .then { doc in
+            print("-: \(doc.title!)")
+
+            manipulateDom(document: doc)
+            
+            return Promise<String>(value: "\(doc.title!) DOM manipulated")
+        }
+        .catch { err in
+            print("-: \(err)")
+        }
+
+        return promise
+    }
+  
     let urlStrings = [
         "https://www.google.com",
         "https://www.microsoft.com",
@@ -69,42 +173,33 @@ func demoErikFutures() {
     
     let urls = urlStrings.map { URL(string: $0)! }
     
-    
     Erik.visit(urlString: urlStrings[0]) { doc, err in
         if err == nil {
-            print("1: \(doc!.title!)")
+            print("1: \(doc!.title)")
         } else {
             print("1: \(err)")
         }
     }
     
-    
-    let _ = erikPromise(url: urls[1]).andThen { result in
-        switch result {
-        case .success(let message):
-            print("2: \(message)")
-            
-        case .failure(let err):
-            print("2. \(err)")
-        }
+    let _ = erikPromise(url: urls[1])
+    .then { message in
+        print("2: \(message)")
+    }
+    .catch { err in
+        print("2. \(err)")
     }
     
-    let _ = Erik.visitFuture(url: urls[2])
-        .andThen { result in
-            switch result {
-            case .success(let doc):
-                print("3: \(doc.title!)")
-                
-            case .failure(let err):
-                print("3: \(err)")
-            }
-        }.andThen { _ in
-            erikPromise(url: urls[3])
-                .onSuccess { message in
-                    print("4: \(message)")
-                }
-                .onFailure { err in
-                    print("4: \(err)")
-                }
-        }
+    let _ = firstly {
+        Erik.visitPromise(url: urls[2])
+    }
+    .then { doc in
+        print("3: \(doc.title)")
+    }.then {
+        erikPromise(url: urls[3])
+    }.then { message in
+        print("4: \(message)")
+    }
+    .catch { err in
+        print("Error: \(err)")
+    }
 }
--- Pods/Target Support Files/Pods-ErikFutureTest/Pods-ErikFutureTest-frameworks.sh
+++ Pods/Target Support Files/Pods-ErikFutureTest/Pods-ErikFutureTest-frameworks.sh
@@ -92,12 +92,14 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/BrightFutures/BrightFutures.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Erik/Erik.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Kanna/Kanna.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/PromiseKit/PromiseKit.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Result/Result.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/BrightFutures/BrightFutures.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Erik/Erik.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Kanna/Kanna.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/PromiseKit/PromiseKit.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Result/Result.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
